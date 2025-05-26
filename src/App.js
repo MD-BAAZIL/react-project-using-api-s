0eb733483abdfaa79a05a0e3425cde98572c581c
BAAZIL <baazilmotors@gmail.com>
2025-05-26 07:17:15 +0530

    api fetching

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [pokemon, setPokemon] = useState("");
+  const [data, setData] = useState(null);
+  const [error, setError] = useState("");
+  const [joke, setJoke] = useState(null)
+
+  const fetchPokemon = async () => {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+      if (!response.ok)
+        throw new Error(`This ${pokemon} pokemon name not found brooooo... Try other name.`);
+      const result = await response.json();
+      setData(result);
+      setError("");
+    } catch (err) {
+      setError(err.message);
+      setData(null);
+    }
+  };
+
+  const fetchJoke = async () => {
+    try{
+      const res = await fetch('https://official-joke-api.appspot.com/random_joke');
+      const joke = await res.json();
+      setJoke(joke);
+      setError("");
+      if(!res.ok){
+        throw new Error("failed to load joke");
+      }
+    }
+    catch (er) {
+      setError(er.message);
+      setJoke({});
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon Finder and generate random jokes Tool By MD BAAZIL</h1>
+      <input
+        type="text"
+        value={pokemon}
+        onChange={(e) => setPokemon(e.target.value)}
+        placeholder="Search Pokemon name here"
+      />
+      <button onClick={fetchPokemon}>Search</button>
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      {data && (
+        <div>
+          <h2>{data.name.toUpperCase()}</h2>
+          <img src={data.sprites.front_default} alt={data.name} />
+          <p>Height: {data.height}</p>
+          <p>Weight: {data.weight}</p>
+          <p>Types: {data.types.map((t) => t.type.name).join(", ")}</p>
+        </div>
+      )}
+      <div>
+        <button onClick={fetchJoke}>Click to generate random jokes</button>
+        {joke && ( <div>
+        <h3>joke type :<strong>{joke.type} </strong></h3>
+        <h3>joke  setup :<strong>{joke.setup}</strong></h3>
+        <h3>joke punch line : <strong>{joke.punchline}</strong></h3>
+        </div> )
+        }
+      </div>
     </div>
+    
   );
 }
 
